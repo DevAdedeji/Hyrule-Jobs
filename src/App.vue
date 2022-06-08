6b4bf1c5e82b4becf1b92c92564c1da0f88efbb3
DevAdedeji <adedejitewogbade2@gmail.com>
2022-06-08 17:31:17 +0100

    Typesscript in Props

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="app">
-    <p>{{ jobs[0].location }}</p>
+    <JobList :jobs="jobs" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref, toRefs } from "vue";
 import Job from "./Types/job";
+import JobList from "./components/JobList.vue";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { JobList },
   setup() {
     const jobs = ref<Job[]>([
       {
@@ -43,26 +44,6 @@ export default defineComponent({
     return {
       jobs,
     };
-    // const state = reactive({
-    //   name: "Link",
-    //   age: 25 as number | string,
-    // });
-    // return {
-    //   ...toRefs(state),
-    // };
-    //   const name = ref("Link");
-    //   const age = ref<number | string>(25);
-    //   return {
-    //     name,
-    //     age,
-    //   };
-    // },
-    // data() {
-    //   return {
-    //     name: "Link",
-    //     age: 25 as number | string,
-    //   };
-    // },
   },
 });
 </script>
